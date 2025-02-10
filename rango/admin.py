05714ad9570c79b64b9122441729e0f79b10a343
Brieuc Doucy <2885025d@student.gla.ac.uk>
2025-02-10 15:10:21 +0000

    chapter 4 and t and admin interface implementation

---
+++ rango/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from rango.models import Category, Page 
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')  # Correct placement
+
+admin.site.register(Page, PageAdmin)  # Register with ModelAdmin
+
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
---
+++ rango/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+##from rango.models import Category
+
+from django.http import HttpResponse
+from rango.models import Category
+
+def index(request):
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+
+# Render the response and send it back!
+    return render(request, 'rango/index.html', context=context_dict)
+
+    
+def about(request):
+    ##return HttpResponse("Rango says here is the about page.<a href='/rango/'>index</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'This tutorial has been put together by brieuc'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context=context_dict)
+   
+def show_category(request, category_name_slug):
+# Create a context dictionary which we can pass
+# to the template rendering engine.
+    context_dict = {}
+    try:
+# Can we find a category name slug with the given name?
+# If we can't, the .get() method raises a DoesNotExist exception.
+# The .get() method returns one model instance or raises an exception.
+        category = Category.objects.get(slug=category_name_slug)
+# Retrieve all of the associated pages.
+# The filter() will return a list of page objects or an empty list.
+        pages = Page.objects.filter(category=category)
+# Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+# We also add the category object from
+# the database to the context dictionary.
+# We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+# We get here if we didn't find the specified category.
+# Don't do anything -
+# the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+# Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context=context_dict)
+   
+def add_category(request):
+    form = CategoryForm()
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+    # Have we been provided with a valid form?
+    if form.is_valid():
+    # Save the new category to the database.
+        form.save(commit=True)
+        # Now that the category is saved, we could confirm this.
+        # For now, just redirect the user back to the index view.
+        return redirect('/rango/')
+    else:
+        # The supplied form contained errors -
+        # just print them to the terminal.
+        print(form.errors)
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
