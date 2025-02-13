909e2b7208ec86d408275bca2b98051d0edeebc1
Brieuc Doucy <2885025d@student.gla.ac.uk>
2025-02-13 20:31:41 +0000

    chapter9

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import UserProfile
 from rango.models import Category, Page 
 
 
@@ -12,3 +13,5 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(UserProfile)
